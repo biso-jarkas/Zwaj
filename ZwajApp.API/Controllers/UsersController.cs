640162ad3af55b0e3017eb1e3fabd33f1b3e1f15
biso-jarkas <bassam-it@hotmail.com>
2020-09-16 04:40:51 -0700

    intiial commit

---
+++ ZwajApp.API/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZwajApp.API.Data;
+using ZwajApp.API.Dtos;
+
+namespace ZwajApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IZwajRepository _repo;
+        private readonly IMapper _mapper;
+        public UsersController(IZwajRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Getuser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            var userToReturn = _mapper.Map<UserForDetailsDto>(user);
+            return Ok(userToReturn);
+        }
+         [HttpPut("{id}")]
+         public async Task<IActionResult> UpdateUser(int id,UserForUpdateDto userForUpdateDto){
+             if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+             return Unauthorized();
+             var userFromRepo = await _repo.GetUser(id);
+             _mapper.Map(userForUpdateDto,userFromRepo);
+             if(await _repo.SaveAll())
+                 return NoContent();
+             
+
+             throw new Exception($"حدثت مشكلة في تعديل بيانات المشترك رقم {id}");
+             
+             
+         }
+
+
+
+    }
+}
---
+++ ZwajApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZwajApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        
+        public string Username { get; set; }
+        [StringLength(8,MinimumLength=4,ErrorMessage="يجب أن لا تقل كلمة السر عن أربعة أحرف ولا تزيد عن ثمانية")]
+        [Required]
+        public string Password { get; set; }
+    }
+}
